7d643c7d0a7049bcdd6987aadde1833365da4781
Marc <marc.artco@gmail.com>
2022-03-01 11:02:48 +0000

    addAction

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -31,39 +31,11 @@ class ApplicationController extends Controller
 
     public function addAction()
     {
-        //call to view
-        
-        // $this->view->render('/task/add.phtml');
-
-        
             // call to view
             // set the layout (template) of the view
             $this->view->setLayout('addFormLayout');
             // render the view using the proper view script
             $this->view->render('/task/add.phtml');
-            // call to model
-            // start the connection to the database
-            $task = new Task();
-            // // get the data from the view
-            
-            $data = array(
-                        'description' => $_POST['description'],
-                        'start' => $_POST['start'],
-                        'end' => $_POST['end'],
-                        'state' => $_POST['state'],
-                        'userFirstName' => $_POST['userFirstName'],
-                        'userLastName' => $_POST['userLastName']
-                    );
-
-            
-            // save them to the database
-            $task->save($data);
-            // redirect to 'index page'
-            header ('location: index');
-        
-    
-
-
     }
 
     // public function saveAction(){
