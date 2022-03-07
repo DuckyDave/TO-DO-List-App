8594e583a705b229934386f641c9506506468e76
Marc <marc.artco@gmail.com>
2022-03-07 12:47:12 +0000

    saveAction

--- app/controllers/indexController.php
+++ app/controllers/indexController.php
@@ -35,11 +35,25 @@
 
 
 
-public function addAction()
-    {
-        // call to view
-            // set the layout (template) of the view
-            $this->view->setLayout('addFormLayout');
-            // render the view using the proper view script
-            $this->view->render('/task/add.phtml');
-    }
+
+
+    
+       
+    //guadar dades del formulari
+            $data = array(
+                'task_description' => $_POST['task_description'],
+                'date_time_start' => $_POST['date_time_start'],
+                'date_time_end' => $_POST['date_time_end'],
+                'task_state' => $_POST['task_state'],
+                'user_first_name' => $_POST['user_first_name'],
+                'user_last_name' => $_POST['user_last_name']
+            );
+
+            
+            //instanciar la classe del model
+            $task = new Task();
+            //invocar el mètode save del model
+            $task->save($data = $data);
+
+            header ('Location: index');
+        
